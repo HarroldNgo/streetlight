2eaba717dc12cb8b5d81e2b0d6e4d2cf7e3baeb1
Lucky2190 <harroldngo@gmail.com>
2023-01-06 18:50:40 -0500

    Changed api link from localhost to new link

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,7 +4,7 @@ import SliderR from "react-slick";
 
 export default function Slider({ posts }) {
 
-    const PF = "http://localhost:5000/images/"
+    const PF = "https://streetlight-api.onrender.com/images/"
     function loop(n) {
         return ([...Array(n)].map((e, i) => posts.map((post, j) => (
             (post.comingsoon ?
