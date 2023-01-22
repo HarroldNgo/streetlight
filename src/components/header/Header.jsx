ff550a367101bac7dea0794ae4459389f69a375c
Lucky2190 <harroldngo@gmail.com>
2023-01-21 23:37:48 -0500

    oops didn't commit this

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,12 @@ import { Link, useLocation } from "react-router-dom"
 import "./header.css"
 import axios from "../../api/axios"
 import { useEffect, useState } from "react"
+import Modal from "../PopUp"
 
 export default function Header() {
     const [cats, setCats] = useState([]);
     const location = useLocation();
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         const getCats = async () => {
@@ -22,32 +24,27 @@ export default function Header() {
                 <a href="https://www.instagram.com/streetlightblog/" target="_blank" rel="noopener noreferrer"><img src={require("../img/insta.png")} alt="instagram" /></a>
             </div>
             <div className="title">
-                <img src={require("../img/titlelogo.png")} alt="titlelogo" />
-                <h1>TREETLIGHT</h1>
-
+                <Link to="/"><img src={require("../img/titlelogo.png")} alt="titlelogo" /></Link>
+                <Link to="/"><h1 className="titletext">TREETLIGHT</h1></Link>
+                <div className="bars-wrapper">
+                    <Link><img src={require("../img/fontbarc.png")} alt="nav-bars" className="bars" onClick={() => setOpenModal(true)} /></Link>
+                    
+                </div>
             </div>
+            <Modal open={openModal} onClose={() => setOpenModal(false)}/>
             <div className="nav">
-                {location.pathname==='/' ?
+                {location.pathname === '/' ?
                     <div className="latest-wrap">
                         <p className="latest">LATEST</p>
-                    </div> : <div className="latest-wrap" style={{opacity: 0}}>
+                    </div> : <div className="latest-wrap" style={{ opacity: 0 }}>
                         <p className="latest">LATEST</p>
                     </div>
-                    }
+                }
 
                 <ul>
-                    <li>
-                        <Link to="/">HOME</Link>
-                    </li>
-                    <li className="cats">
-                        <Link to="/categories">CATEGORIES</Link>
-                        <ul>
-                            {cats.map((c, i) => (
-                                <li key={i}><Link to={`/categories/?cat=${c.name}`}>{c.name}</Link></li>
-                            ))}
-                        </ul>
-                    </li>
-                    <li><Link to="/about">ABOUT</Link></li>
+                    <li><Link className="nav-option" to="/">HOME</Link></li>
+                    <li className="cats"><Link className="nav-option" to="/categories">CATEGORIES</Link></li>
+                    <li><Link className="nav-option" to="/about">ABOUT</Link></li>
                 </ul>
             </div>
         </div>
