e7d083adf3d7bfb09083eefc4397162aa23ca6ee
Lucky2190 <harroldngo@gmail.com>
2023-01-28 19:16:52 -0500

    re-added image caption sizing

--- src/components/singlepost/SinglePost.jsx
+++ src/components/singlepost/SinglePost.jsx
@@ -11,34 +11,32 @@ export default function SinglePost() {
 
     useEffect(() => {
         const getPost = async () => {
-            const res = await axios.get("/api/posts/" + path)
+            const res = await axios.get("/api/posts?id=" + path)
             setPost(res.data)
         };
         getPost();
     }, [path]);
 
-    /*
-    <p className="desc">{p.desc.split(/\r?\n/).slice(0, p.desc.split(/\r?\n/).length / 2).join("\n")}
-                            <br />
-                            <span className="desc-secondary">{(p.desc).split(/\r?\n/).slice(p.desc.split(/\r?\n/).length / 2, p.desc.split(/\r?\n/).length).join("\n")}</span>
-                        </p>
-    */
 
     return (
         <div className="outer">
-            <div className="singlepost">
-                <div className="postimage" id="postimage">
-                    {post.photo && (
-                        <img src={PF + post.photo + ".png"} alt="" className="single-postimage" />
-                    )}
-                    <p className="desc">{post.desc}
-                    </p>
+            {post.map((p, i) => (
+                <div key={i} className="singlepost">
+                    <div className="postimage" id="postimage">
+                        {p.photo && (
+                            <img src={PF + p.photo + ".png"} alt="" className="single-postimage" />
+                        )}
+                        <p className="desc">{p.desc.split(/\r?\n/).slice(0, p.desc.split(/\r?\n/).length / 2).join("\n")}
+                            <br />
+                            <span className="desc-secondary">{(p.desc).split(/\r?\n/).slice(p.desc.split(/\r?\n/).length / 2, p.desc.split(/\r?\n/).length).join("\n")}</span>
+                        </p>
+                    </div>
+                    <div className="postbody">
+                        <h1 className="post-title">{p.title}</h1>
+                        <p className="paragraph">{p.body}</p>
+                    </div>
                 </div>
-                <div className="postbody">
-                    <h1 className="post-title">{post.title}</h1>
-                    <p className="paragraph">{post.body}</p>
-                </div>
-            </div>
+            ))}
         </div>
 
     )
